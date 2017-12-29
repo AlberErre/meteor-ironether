252b9a4de0d2176098439954913239d12f14c68b
Alber Erre <arblugo@gmail.com>
2017-12-29 01:02:44 +0000

    a

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'Show Accounts without ether unit',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/AlberErre/meteor-ironether.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
