eb68c1d424c3735530ce20a9e7f9716834eb66c6
Alber Erre <arblugo@gmail.com>
2017-12-29 02:33:45 +0000

    ecmascript version

--- package.js
+++ package.js
@@ -1,18 +1,18 @@
 Package.describe({
   name: 'albererre:ironether',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'Show Accounts without ether unit',
   // URL to the Git repository containing the source code for this package.
   git: 'https://github.com/AlberErre/meteor-ironether.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md'
+  documentation: null
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.6.0.1');
-  api.use('ecmascript');
+  api.use('ecmascript@0.4.3');
   api.mainModule('ironether.js');
   api.addAssets('selectAccount_iron.html', 'client');
   api.addFiles('selectAccount_iron.js', 'client');
@@ -20,7 +20,7 @@ Package.onUse(function(api) {
 
 
 Package.onTest(function(api) {
-  api.use('ecmascript');
+  api.use('ecmascript@0.4.3');
   api.use('tinytest');
   api.use('albererre:ironether');
   api.mainModule('ironether-tests.js');
